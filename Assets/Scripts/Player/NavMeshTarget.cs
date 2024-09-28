530041327801de169ff67981ce6d8a92fb32a20f
ricardoorozco <ricardoorozco@users.noreply.github.com>
2024-09-27 20:37:52 -0500

    add nav mesh 2d

---
+++ Assets/Scripts/Player/NavMeshTarget.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshTarget : MonoBehaviour
+{
+    [SerializeField] private bool lookingRight;
+
+    private NavMeshAgent agent;
+    private Camera cam;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray);
+
+            if (hit.collider != null)
+            {
+                SetDestination(hit.point);
+            }
+        }
+    }
+
+    private void SetDestination(Vector3 destination)
+    {
+        agent.SetDestination(destination);
+        Vector3 direction = (destination - transform.position).normalized;
+        if (direction.x > 0 && !lookingRight)
+        {
+            Flip();
+        }
+        else if (direction.x < 0 && lookingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        lookingRight = !lookingRight;
+        transform.Rotate(0, 180, 0);
+    }
+}
