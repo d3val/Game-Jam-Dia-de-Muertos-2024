b0e11f84cead1103db1003dced031d837ef8c656
d3val <devalt5008@gmail.com>
2024-09-27 19:19:12 -0600

    Basic room translation system

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    [Tooltip("The room number where is placed this door")]
     [SerializeField] int room;
+    [Tooltip("The other side door connected to this one")]
     [SerializeField] Door connectedDoor;
+    [Tooltip("Where the player will be spawned when cross to this door")]
     public Transform spawnPos;
-
 
     void Pass(GameObject item)
     {
@@ -16,7 +18,10 @@ public class Door : MonoBehaviour
             Debug.LogError("Connected door not founded");
             return;
         }
+
+        // We move the player to the connected doos spawn position and init the translation of the camera.
         item.transform.position = connectedDoor.spawnPos.position;
+        CameraMovement.Instance.MoveToRoom(connectedDoor.room);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
